29ad45ae01a2139b666b713193aca4bc0258a284
fatima774 <fatimabougrin146@gmail.com>
2025-06-12 13:29:50 +0200

    gy

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Accueil | Atlantique Hygiène</title>
+    <title><?= $pageTitle ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="accueil.css" />
@@ -52,7 +52,7 @@
     <header>
       <div class="header-container">
         <img src="images/logo.png" alt="Logo Atlantique Hygiène" class="logo" />
-        <h1>Atlantique Hygiène</h1>
+        <h1><?= $pageTitle ?></h1>
       </div>
       <nav>
         <ul>
